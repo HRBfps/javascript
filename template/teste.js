81cc35d8b8491208ca452b65e268ae9674e10f6b
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-02-27 12:16:20 -0300

    eita

---
+++ template/teste.js
@@ -0,0 +1,6 @@
+let soma1 = 10
+let soma2 = 14
+
+let soma = soma1 + soma2
+
+console.log(soma)
---
+++ template/variaveis.js
@@ -0,0 +1,24 @@
+/* var idad = 14 // var não é clean code
+let idade = 14 // let deixa vc mudar depois
+
+idade = 213
+const idadee = 14 // const e let sao parecidos
+
+let nome = "Henri :)" // String
+let numero = 402 // Int
+let numeroFlutuante = 2.58 // Float
+let condicao = true // boolean
+
+console.log(typeof nome)
+console.log(typeof numero)
+console.log(typeof numeroFlutuante)
+console.log(typeof condicao)
+*/
+
+let soma1 = prompt("Numerozinho: ")
+let soma2 = prompt("Digita ai rei: ")
+
+let soma = soma1 + soma2
+
+//console.log(soma)
+document.write(soma)
