c065e5c814d6c18698247094d29fe5216130b166
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-03-06 12:04:03 -0300

    variar cardapio

--- template/variaveis.js
+++ template/variaveis.js
@@ -23,30 +23,63 @@ console.log(typeof condicao)
 
 let quantidade = prompt("quantidade de porções(100g) de jabuticaba")
 
-let energ = document.querySelector('.energ')
-let carbo = document.querySelector('.carbo')
-let prote = document.querySelector('.prote')
-let gT = document.querySelector('.gT')
-let fibr = document.querySelector('.fibr')
-let vitC = document.querySelector('.vitC')
-let mag = document.querySelector('.mag')
-
-let qnt = quantidade*15.3
-qnt = qnt.toFixed(2)
-energ.innerHTML = "Energia: "+ quantidade*58 +" Kcal",
-carbo.innerHTML = "Carboidratos: "+qnt+" g",
-qnt = quantidade*0.6
-qnt = qnt.toFixed(2)
-prote.innerHTML = "Proteinas: "+qnt + " g",
-qnt = quantidade*0.1
-qnt = qnt.toFixed(2)
-gT.innerHTML = "Gorduras totais: "+qnt + " g",
-qnt = quantidade*2.3
-qnt = qnt.toFixed(2)
-fibr.innerHTML = "Fibras: "+qnt + " g",
-qnt = quantidade*16.2
-qnt = qnt.toFixed(2)
-vitC.innerHTML = "Vitamina C: "+qnt + " mg",
-qnt = quantidade*18.0
-qnt = qnt.toFixed(2)
-mag.innerHTML = "Magnésio: "+ qnt + " mg"
+if (quantidade > 8) {
+    let energ = document.querySelector('.energ')
+    let carbo = document.querySelector('.carbo')
+    let prote = document.querySelector('.prote')
+    let gT = document.querySelector('.gT')
+    let fibr = document.querySelector('.fibr')
+    let vitC = document.querySelector('.vitC')
+    let mag = document.querySelector('.mag')
+    let texto = document.querySelector('.texto')
+    
+    let qnt = quantidade*15.3
+    qnt = qnt.toFixed(2)
+    energ.innerHTML = "Energia: "+ quantidade*58 +" Kcal",
+    carbo.innerHTML = "Carboidratos: "+qnt+" g",
+    qnt = quantidade*0.6
+    qnt = qnt.toFixed(2)
+    prote.innerHTML = "Proteinas: "+qnt + " g",
+    qnt = quantidade*0.1
+    qnt = qnt.toFixed(2)
+    gT.innerHTML = "Gorduras totais: "+qnt + " g",
+    qnt = quantidade*2.3
+    qnt = qnt.toFixed(2)
+    fibr.innerHTML = "Fibras: "+qnt + " g",
+    qnt = quantidade*16.2
+    qnt = qnt.toFixed(2)
+    vitC.innerHTML = "Vitamina C: "+qnt + " mg",
+    qnt = quantidade*18.0
+    qnt = qnt.toFixed(2)
+    mag.innerHTML = "Magnésio: "+ qnt + " mg"
+    texto.innerHTML = "A quantidade de calorias representa "+ quantidade*3+"% do valor diário, mais do que 25%, melhor mudar de cardápio."
+} else {
+    let energ = document.querySelector('.energ')
+    let carbo = document.querySelector('.carbo')
+    let prote = document.querySelector('.prote')
+    let gT = document.querySelector('.gT')
+    let fibr = document.querySelector('.fibr')
+    let vitC = document.querySelector('.vitC')
+    let mag = document.querySelector('.mag')
+    let texto = document.querySelector('.texto')
+    
+    let qnt = quantidade*15.3
+    qnt = qnt.toFixed(2)
+    energ.innerHTML = "Energia: "+ quantidade*58 +" Kcal",
+    carbo.innerHTML = "Carboidratos: "+qnt+" g",
+    qnt = quantidade*0.6
+    qnt = qnt.toFixed(2)
+    prote.innerHTML = "Proteinas: "+qnt + " g",
+    qnt = quantidade*0.1
+    qnt = qnt.toFixed(2)
+    gT.innerHTML = "Gorduras totais: "+qnt + " g",
+    qnt = quantidade*2.3
+    qnt = qnt.toFixed(2)
+    fibr.innerHTML = "Fibras: "+qnt + " g",
+    qnt = quantidade*16.2
+    qnt = qnt.toFixed(2)
+    vitC.innerHTML = "Vitamina C: "+qnt + " mg",
+    qnt = quantidade*18.0
+    qnt = qnt.toFixed(2)
+    mag.innerHTML = "Magnésio: "+ qnt + " mg"
+}
