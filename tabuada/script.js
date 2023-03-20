9e23cb970b00c6c4f838d8ce7a3bd48c13415430
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-03-20 12:25:31 -0300

    tabuada

---
+++ tabuada/script.js
@@ -0,0 +1,5 @@
+let n = parseInt(prompt("Digite o numero da tabuada"))
+for(i = 1; i<=10; i++)
+    document.write('<section>'+n+' x '+i+' =', n*i +'</section>')
+
+document.write('<video src="https://www.youtube.com/watch?v=dSvQnPtKiVg" controls ></video>')
