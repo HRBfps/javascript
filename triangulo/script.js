1ff39f058a9ff01e3a2efe803d2d1596ce80e33e
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-03-20 11:33:34 -0300

    eitas

--- triangulo/script.js
+++ triangulo/script.js
@@ -7,12 +7,18 @@ let lado1 = parseInt(prompt("Qual a largura do primeiro lado do seu triangulo?")
 let lado2 = parseInt(prompt("Qual a largura do segundo lado do seu triangulo?"))
 let lado3 = parseInt(prompt("Qual a largura do terceiro lado do seu triangulo?"))
 
-if ((lado1 == lado2) && (lado1 == lado3)){
-    document.write('<section class="sec"> <p>Equilatero(Tudo Igual)</p> <img class="triangulo" src="./images/equilatero.png"></section>') 
-}else if ((lado1 !== lado2) && (lado2 !== lado3) && (lado1 !== lado3)) {
-    document.write('<section class="sec"><p>Isósceles (Todos diferentes) </p><img class="triangulo" src="./images/escaleno.png"></section>') 
-}else{
-    document.write('<section class="sec"><p>Escaleno(Só dois iguáis)</p><img class="triangulo" src="./images/isósceles.png"></section>') 
-}
+if (lado1 <= lado2 + lado3 &&
+    lado2 <= lado3 + lado1 &&
+    lado3 <= lado1 + lado2) {
+        if ((lado1 == lado2) && (lado1 == lado3)){
+            document.write('<p>Equilatero(Tudo Igual)</p> <section class="sec"><img class="triangulo" src="./images/equilatero.png"></section>') 
+        }else if ((lado1 !== lado2) && (lado2 !== lado3) && (lado1 !== lado3)) {
+            document.write('<p>Escaleno(Todos iguais)</p> <section class="sec"> <img class="triangulo" src="./images/escaleno.png"></section>') 
+        }else{
+            document.write('<p>Isósceles(Só dois iguais)</p> <section class="sec"> </p><img class="triangulo" src="./images/isósceles.png"></section>') 
+        }
+    }else {
+        document.write('<p class="erro">isso nem é triangulo bobao</p>')
+    }
 /*
 document.write('<section class="sec"><p></p><img class="triangulo" src="./images/triangulo.png"></section>') */
