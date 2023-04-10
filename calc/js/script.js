a7c532174bac0f7f78a0116406dc16bcf4d7113f
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-04-10 12:25:23 -0300

    calc

---
+++ calc/js/script.js
@@ -0,0 +1,29 @@
+//calculadora em js
+
+function soma() {
+    let x = parseInt(document.getElementById('n1').value)
+    let y = parseInt(document.getElementById('n2').value)
+
+    return document.getElementById('resultado').value = x+y
+}
+
+function sub() {
+    let x = parseInt(document.getElementById('n1').value)
+    let y = parseInt(document.getElementById('n2').value)
+
+    return document.getElementsById('resultado').value = x-y
+}
+
+function div() {
+    let x = parseInt(document.getElementById('n1').value)
+    let y = parseInt(document.getElementById('n2').value)
+
+    return document.getElementById('resultado').value = x/y
+}
+
+function mult() {
+    let x = parseInt(document.getElementById('n1').value)
+    let y = parseInt(document.getElementById('n2').value)
+
+    return document.getElementById('resultado').value = x*y
+}
